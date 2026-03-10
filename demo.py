23367b68eea4a9a7377d40e22b97a0f3e38826dc
Dharshiniganesan15 <dharshiniganesan1515@gmail.com>
2026-03-10 11:23:25 +0530

    success

--- demo.py
+++ demo.py
@@ -1,13 +1,4 @@
-def calculator(a, b):
+def add(a,b):
     return a + b
-def subtract(a, b):
+def subtract(a,b):
     return a - b
-def multiply(a, b):
-    return a * b
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
-def palindrome(s):
-    return s == s[::-1]
-    
